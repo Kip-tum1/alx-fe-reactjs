3f2bf200622c518a77a381b0cb801d3599e3743d
Kiptum <duncankipkoech12@gmail.com>
2025-09-13 15:30:32 +0300

    Zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import RecipeList from "./Components/RecipeList";
+import AddRecipeForm from "./Components/AddRecipeForm";
+
+function App(){
+  return(
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  )
+}
+
+export default App;
