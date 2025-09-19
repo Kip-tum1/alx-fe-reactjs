9afa4d7bdb27c50bbd7e6eb06dfb26c15369a6f4
Kiptum <duncankipkoech12@gmail.com>
2025-09-19 15:12:10 +0300

    apis

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,15 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from "./components/Home";
+import axios from 'axios';
 
 function App(){
   return(
     <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       
     </div>
   )
