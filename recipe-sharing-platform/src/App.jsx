60c70982c3abf7307bb15f69dd1d7464440e758f
Kiptum <duncankipkoech12@gmail.com>
2025-10-05 08:57:04 +0300

    recipr

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,15 @@
-import HomePage from './components/Homepage'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import HomePage from './components/HomePage';
+import HomePage from './components/Homepage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <>
-    <HomePage />
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
