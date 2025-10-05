bcb1edcf25de4243b30f33f4b9dbf3fe7f2fb71c
Kiptum <duncankipkoech12@gmail.com>
2025-10-05 15:22:32 +0300

    add

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import HomePage from './components/HomePage';
 import HomePage from './components/Homepage';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path= "/addrecipeform" element={<AddRecipeForm />}/>
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   );
 }
+export default App;
